2e3dc02b46d2436bce1f7748358834a93f75cf22
unknown <D:\software\The Bat!\Mail>
2009-03-25 23:08:55 +0800

    add campus roamer

---
+++ public/campus_roamer/campus_roamer.js
@@ -0,0 +1,107 @@
+var jsReady = false;
+
+var usernames = {};
+var thumbnails = {};
+var selectedID = '';
+
+function isReady() {
+    return jsReady;
+}
+
+function sendToActionScript(value) {
+    flashAPI.thisFlash().sendToActionScript(value);
+}
+
+function getProfilePath() {
+    // return "http://www.yiqi.com/home.php?tab=parlor&uid=";
+    return "http://xiaonei.com/profile.do?id=";
+}
+
+function loadFriends(uid) {
+    var req = opensocial.newDataRequest();
+
+    if(uid == null) {
+        uid = "VIEWER";
+    }
+    else {
+        var viewer = opensocial.newIdSpec({
+            "userId" : uid,
+            "groupId" : "SELF"
+        });
+        var opt_params = {};
+        req.add(req.newFetchPeopleRequest(viewer, opt_params), 'viewer');
+        selectedID =  uid;
+    }
+    req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.VIEWER), 'viewer');
+
+    var viewerFriends = opensocial.newIdSpec({
+        "userId" : uid,
+        "groupId" : "FRIENDS"
+    });
+    var opt_params = {};
+    opt_params[opensocial.DataRequest.PeopleRequestFields.MAX] = 1000;
+    // TOP_FRIENDS, NAME
+    opt_params[opensocial.DataRequest.PeopleRequestFields.SORT_ORDER] = opensocial.DataRequest.SortOrder.TOP_FRIENDS;
+    // TOP_FRIENDS, ALL
+    // opt_params[opensocial.DataRequest.PeopleRequestFields.FILTER] = opensocial.DataRequest.FilterType.ALL;
+    req.add(req.newFetchPeopleRequest(viewerFriends, opt_params), 'viewerFriends');
+
+    req.send(onLoadFriends);
+}
+
+function onLoadFriends(data) {
+    var viewerFriends = data.get('viewerFriends').getData();
+
+    var uid = "";
+    if(selectedID) {
+        uid = selectedID;
+    }
+    else {
+        var viewer = data.get('viewer').getData();
+        uid = viewer.getId();
+        usernames[uid] = viewer.getDisplayName();
+        thumbnails[uid] = viewer.getField('thumbnailUrl');
+    }
+
+    // construct xml for flex component
+    var index = 0;
+    var friends_xml = "<person><id>" + uid + "</id><name>" + usernames[uid] + "</name><thumbnail>" + thumbnails[uid] + "</thumbnail><friends>";
+    viewerFriends.each( function(person) {
+        var id = person.getId();
+        usernames[person.getId()] = person.getDisplayName();
+        thumbnails[person.getId()] = person.getField('thumbnailUrl');
+
+        if(id) {
+            friends_xml += "<friend index='" + index.toString() + "'><id>" + id + "</id><name>" + usernames[id] + "</name><thumbnail>" + thumbnails[id] + "</thumbnail></friend>";
+            index = index + 1;
+        }
+    });
+    friends_xml += "</friends></person>";
+
+//    prompt("All friends", friends_xml);
+    sendToActionScript(friends_xml);
+}
+
+
+function init() {
+    jsReady = true;
+
+    // load spring graph
+    var swf_path = "http://web93131016.w1.web335.net.cn/campus_roamer/CampusRoamer.swf"+ "?" + Math.random().toString();
+    flashAPI.embedFlash(swf_path + "?" + Math.random().toString(),
+        "flash_container", "9", {
+            width: 790,
+            height: 470,
+            quality: "high",
+            allowScriptAccess: "always"
+        });
+        // 790x470, or 850x600,
+}
+
+function reset() {
+    usernames = {};
+    thumbnails = {};
+    selectedID = '';
+}
+
+gadgets.util.registerOnLoadHandler(init);
