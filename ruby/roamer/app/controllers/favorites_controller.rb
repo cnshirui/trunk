8b812b771960316f637d6d7c111ff49ef3958688
shirui <shiruide@gmail.com>
2009-03-26 01:16:02 +0800

    merge github to git

--- ruby/roamer/app/controllers/favorites_controller.rb
+++ ruby/roamer/app/controllers/favorites_controller.rb
@@ -22,9 +22,19 @@ class FavoritesController < ApplicationController
   end
 
   def list
-    favorites = Favorite.find(:all, :conditions => "host_uid = #{params[:id]}")
+    favorites = Favorite.find(:all, :conditions => "host_uid like '#{params[:id]}'")
+    require 'pp'
+    p "^^^^^^^^^^^^^^^^^^^^^^^^"
+    p favorites.inspect
     users = favorites.map { |item|  User.find_by_uid(item.guest_uid) }
-    users = users ? users.uniq!.compact! : []
+    p "^^^^^^^^^^^^^^^^^^^^^^^^"
+    p users.inspect
+    users = users ? users.uniq : []
+    p "^^^^^^^^^^^^^^^^^^^^^^^^"
+    p users.inspect
+    users = users ? users.compact : []
+    p "^^^^^^^^^^^^^^^^^^^^^^^^"
+    p users.inspect
     render :xml => users.to_xml
   end
 
---
+++ ruby/typo-5.2/themes/metallic/about.markdown
@@ -0,0 +1,5 @@
+### Metallic
+
+Typo's theme by [Piotr Polak][1]
+
+[1]: http://www.piotrpolak.com/ "piotrpolak.com"
