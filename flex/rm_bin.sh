9b74e9f86b5ead246326ce25ce6e0eb29ff2336a
unknown <rshi@.dhcp.pgdev.sap.corp>
2009-04-17 15:07:16 +0800

    'a'

--- flex/rm_bin.sh
+++ flex/rm_bin.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-#ls -1 | while read f; do java YourClass $f > "$f.new"; done
-ls */bin*/* | 
-while read f; 	do
-		echo $f;
-		if [ -d $f ]
-		then
-			echo ---------------- is a directory !;
-		else
-			rm $f;
-			echo $f OK !;
-		fi	
-done
+# ls -1 | while read f; do java YourClass $f > "$f.new"; done
+# ls -1 | while read f; do echo $f; done
+# ls */bin*/* | 
+# while read f; 	
+	# do 	echo $f;
+	# if [ -d $f ]
+	# then
+		# echo ---------------- is a directory !;
+	# else
+		# rm $f;
+		# echo $f OK !;
+	# fi
+# done
+
+ls */bin*/* | while read f;  do rm -rf $f; echo $f; done
