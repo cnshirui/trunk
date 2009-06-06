0b660a170489aa963b7767e1f530a3cb026c08a0
shirui <shiruide@gmail.com>
2009-06-06 08:38:17 +0800

    add

---
+++ cpp/ProgrammingBeauty/ProgrammingBeauty/ProgrammingBeauty.cpp
@@ -0,0 +1,17 @@
+// ProgrammingBeauty.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+#include "PointsGame.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//cout<< "shirui" << endl;
+	PointsGame();
+	return 0;
+}
+
