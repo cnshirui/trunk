09f357db47e16b96f06c0bd4c0ba00a9b49be68c
shirui <shiruide@gmail.com>
2009-07-16 01:22:43 +0800

    add

---
+++ cpp/LokiTest/LokiTest/LokiTest.cpp
@@ -0,0 +1,15 @@
+// LokiTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "static_check.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << sizeof(void*) << endl;
+	cout << sizeof(char) << endl;
+
+	return 0;
+}
+
