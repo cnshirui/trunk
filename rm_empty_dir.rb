d71a287be9585a4325b8e331402b03e0a386659e
shirui <shiruide@gmail.com>
2009-08-11 22:52:54 +0800

    rm_dir

--- rm_empty_dir.rb
+++ rm_empty_dir.rb
@@ -5,8 +5,14 @@ $level_count = 0
 def rm_empty_dir(path)
 
 	return unless(File.directory?(path))
-	Dir.chdir(path)
-  $level_count += 1
+	
+	begin
+		Dir.chdir(path)
+	rescue
+		puts "can't chdir to #{path}"
+		return
+	end
+	$level_count += 1
 	#puts "<-#{'---'*$level_count} #{path}"
 
 
@@ -18,15 +24,15 @@ def rm_empty_dir(path)
 
 	# do delete
 	#puts "#{'---'*$level_count}-> #{path}"
-  if(Dir["*"].length == 0)
-      puts "FileUtils.rm_rf '#{Dir.pwd}'"
-      FileUtils.chmod 0777, Dir.pwd
-      #Dir.rmdir(Dir.pwd)
-      FileUtils.rm_rf(Dir.pwd)
-  end
+	if(Dir["*"].length == 0)
+	  puts "FileUtils.rm_rf '#{Dir.pwd}'"
+	  FileUtils.chmod 0777, Dir.pwd
+	  #Dir.rmdir(Dir.pwd)
+	  FileUtils.rm_rf(Dir.pwd)
+	end
 
-  Dir.chdir("..")
-  $level_count -= 1
+	Dir.chdir("..")
+	$level_count -= 1
 end
 
 rm_empty_dir("d:\\perforce\\git\\trunk")
