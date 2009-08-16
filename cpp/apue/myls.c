480a19e9f4edf29e5cae3b8e725bb5ef5f331567
shirui <shiruide@gmail.com>
2009-08-16 21:54:23 +0800

    'add'

---
+++ cpp/apue/myls.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main_myls(int argc, char* argv[])
+{
+    DIR *dp;
+    struct dirent *dirp;
+
+    if(argc != 2)
+    {
+        printf("You need input the directory name.\n");
+        exit(1);
+    }
+
+    if((dp = opendir(argv[1])) == NULL)
+    {
+        printf("cannot open %s\n", argv[1]);
+        exit(1);
+    }
+
+    while ((dirp = readdir(dp)) != NULL)
+        printf("%s\n", dirp->d_name);
+
+    closedir(dp);
+
+    exit(0);
+}
